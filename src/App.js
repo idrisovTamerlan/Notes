32a3b02c49ecadb4be04ede00a7a61b5d7f8ac79
Tamerlan <weelsayd@gmail.com>
2021-03-21 19:27:02 +0300

    First load

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './styles.css';
 function App() {
+    const todos = [
+        {text: "Найти камень души",
+            favorite: false },
+
+        { text: "Собрать остальные камни бесконечности",
+            favorite: true },
+
+        {text: "Щелкнуть",
+            favorite: true }
+        ];
+
+    const NewTodos = todos.map((todo) =>{
+
+        let todoClass = `todo ${todo.favorite ? 'selected' : ''}`
+
+        const deleteTodo = () => {
+            alert(" Какой ценой?")
+        }
+
+
+
+return(
+     <div className={todoClass}>
+             <div className='favorite'>
+                    <span> ★ </span>
+             </div>
+
+            <div className='text'>
+               {todo.text}
+            </div>
+
+            <div className='delete'>
+                     <button onClick={deleteTodo}>
+                         ✖
+                     </button>
+            </div>
+
+        </div>)
+    })
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+     <div className='header'>СПИСОК ДЕЛ</div>
+
+     <div className='form'>
+    <input placeholder="Введите текст..." type='text'/>
+        <button>
+            Добавить
+        </button>
+     </div>
+
+     <div className='todos'>
+         {NewTodos}
+     </div>
+
+
+
     </div>
   );
 }
